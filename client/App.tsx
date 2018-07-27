682011e052ca8da0fc1415657073fe696fa7aa2a
leejh3224 <leejh3224@naver.com>
2018-07-27 13:28:17 +0900

    add jest setups and absolute-path import

--- client/App.tsx
+++ client/App.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
+import A from 'components/a'
+
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
+        <A />
       </View>
     )
   }
---
+++ client/src/components/a.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import { View, Text } from 'react-native'
+
+const A = () => {
+  return (
+    <View>
+      <Text>aaa</Text>
+    </View>
+  )
+}
+
+export default A
