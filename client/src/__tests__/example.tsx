6f96dcb3028acfc8d6476f0031af1ec4b2b95c6f
leejh3224 <leejh3224@naver.com>
2018-07-27 13:10:10 +0900

    add jest setup and absolute path resolution

---
+++ client/src/__tests__/example.tsx
@@ -0,0 +1,5 @@
+describe('hello?', () => {
+  it('works', () => {
+    expect(1).toBe(1)
+  })
+})
--- server/api/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router, Request, Response } from 'express'
-
-const router: Router = Router()
-
-router.get('/app', (req: Request, res: Response) => {
-  res.send('hi!')
-})
-
-export default router
